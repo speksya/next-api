f0baf5e7a8f22f5daaad27fce479cd5b232d79a4
speksya <vanya.tar@gmail.com>
2025-04-02 19:51:19 +0300

    refactor: rename PostLayout dir to Posts

--- src/pages/Posts/component.tsx
+++ src/pages/Posts/component.tsx
@@ -1,7 +1,7 @@
 import type { Tag } from "@prisma/client";
 import type { FC } from "react";
 
-import { PostLayout } from "@/shared/layouts/PostLayout";
+import { PostLayout } from "@/shared/layouts/Posts";
 import { PostsWidget } from "@/widgets/Posts";
 
 type Props = {
